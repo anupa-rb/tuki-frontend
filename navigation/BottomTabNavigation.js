ec94631dbb5e0c14859457f7a889116f8a9fd102
Anupa Ranabhat <anupa.ranabhat@gmail.com>
2025-01-20 23:56:16 +0545

    added buyer and seller folder

--- navigation/BottomTabNavigation.js
+++ navigation/BottomTabNavigation.js
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Home';
-import Search from '../screens/Search';
-import Profile from '../screens/Profile';
+import Home from '../screens/Buyer/Home';
+import Search from '../screens/Buyer/Search';
+import Profile from '../screens/Buyer/Profile';
 import { Ionicons } from "@expo/vector-icons"
 
 
